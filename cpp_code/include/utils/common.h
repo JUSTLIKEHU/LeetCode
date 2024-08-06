863eca29a2eab164596fa669000e2763ba6727aa
justlikehu <1151217347@qq.com>
2024-08-06 23:31:36 +0800

    add register macro

---
+++ cpp_code/include/utils/common.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "easy/solution.h"
+
+#define REGISTER_SOLUTION_EASY_FUNCTION(global_obj, func_name, func_ptr) \
+  do {                                                       \
+    global_obj.registerFunction(#func_name, func_ptr);    \
+  } while (0) \
+
+#define REGISTER_SOLUTION_EASY_STATIC_FUNCTION(global_obj, func_name, func_ptr) \
+  do {                                                             \
+    global_obj.registerStaticFunction(#func_name, func_ptr);    \
+  } while (0)
--- cpp_code/src/easy/main.cpp
+++ cpp_code/src/easy/main.cpp
@@ -1,20 +1,24 @@
 #include "easy/solution.h"
+#include "utils/common.h"
 #include <exception>
 
+auto& g_solution_easy = Solution_easy::getInstance();
 int main(int argc, char** argv) {
   
 
-  auto& solution_easy = Solution_easy::getInstance();
-  // print(solution_easy.romanToInt("IV"));
+  // auto& solution_easy = Solution_easy::getInstance();
 
-  solution_easy.registerFunction("romanToInt", &Solution_easy::romanToInt);
+  // solution_easy.registerFunction("romanToInt", &Solution_easy::romanToInt);
 
-  solution_easy.registerStaticFunction("isPalindrome", &Solution_easy::isPalindrome);
+  // solution_easy.registerStaticFunction("isPalindrome", &Solution_easy::isPalindrome);
+
+  REGISTER_SOLUTION_EASY_FUNCTION(g_solution_easy, romanToInt, &Solution_easy::romanToInt);
+  REGISTER_SOLUTION_EASY_STATIC_FUNCTION(g_solution_easy, isPalindrome, Solution_easy::isPalindrome);
 
   try {
-    auto res = solution_easy.callFunction<int, std::string>("romanToInt", "IV");
+    auto res = g_solution_easy.callFunction<int, std::string>("romanToInt", "IV");
     print(res);
-    auto res2 = solution_easy.callStaticFunction<bool, int>( "isPalindrome", 100);
+    auto res2 = g_solution_easy.callStaticFunction<bool, int>( "isPalindrome", 100);
     print(res2);
   } catch (const std::exception &e) {
     std::cout << e.what() << '\n';
